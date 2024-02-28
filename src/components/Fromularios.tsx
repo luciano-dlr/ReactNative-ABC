103a0b07c9cd40529f49f01e356338f7d6222eb4
luciano-dlr <delarubialuciano@gmail.com>
2024-02-28 18:50:56 -0300

    zustand logic login type

--- src/components/Fromularios.tsx
+++ src/components/Fromularios.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { useForm } from '../hooks/useForm';
+import { useAuthStore } from '../store/auth.store';
 
 export const Fromularios = () => {
 
@@ -6,6 +8,31 @@ export const Fromularios = () => {
         email: 'test@test.com',
         password: '123456'
     })
+
+    const authStatus = useAuthStore(state => state.status)
+    const user = useAuthStore(state => state.user)
+
+
+    const login = useAuthStore(state => state.login)
+    const logout = useAuthStore(state => state.logout)
+
+    useEffect(() => {
+
+        setTimeout(() => {
+
+            logout()
+
+        }, 2000);
+
+
+    }, [])
+
+
+
+
+    if (authStatus === 'checking') {
+        return <h3>Loading ..</h3>
+    }
 
     return (
         <>
@@ -28,6 +55,24 @@ export const Fromularios = () => {
             <code>
                 <pre>{JSON.stringify(formulario, null, 2)}</pre>
             </code>
+
+            <h3>Login Page</h3>
+            {(authStatus === 'authenticated')
+
+                ? <h6>Autenticado como : {JSON.stringify(user, null, 2)}</h6>
+                : <h6>No autenticated brother</h6>
+
+            }
+            <h6>{authStatus}</h6>
+
+            {(authStatus === 'authenticated')
+
+                ? <button onClick={logout}>LogOut</button>
+                : <button onClick={() => login('leonardito@gmail.com', '123')} >Login</button>
+
+            }
+
+
         </>
     )
 }
---
+++ src/store/auth.store.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+
+interface AuthState {
+    status: 'authenticated' | 'unautheticated' | 'checking';
+    token?: string;
+    user?: {
+        name: string;
+        email: string;
+    }
+
+    login:(email:string,password:string) => void;
+    logout:()=> void;
+    
+
+}
+
+export const useAuthStore = create<AuthState>()((set) => ({
+
+    status: 'checking',
+    token: undefined,
+    user: undefined,
+    login:(email:string,password:string) => {
+
+        set({
+            status:'authenticated',
+            token:'ABC123',
+            user:{
+                name:'Leonardito',
+                email:email
+            }
+        })
+
+    },
+
+    logout:() => {
+        set({
+        status:'unautheticated',
+        token:undefined,
+        user:undefined
+    })
+    }
+
+}));
